b8648bcb5634ec8a57ac92199ff1a7b4baddf84b
Peter Staev <peter@tangrasoft.com>
2018-08-07 19:01:11 +0300

    fix correctly redraw widget on change of column/row size

--- grid-view-common.ts
+++ grid-view-common.ts
@@ -182,6 +182,7 @@ const defaultRowHeight: PercentLength = "auto";
 export const rowHeightProperty = new CoercibleProperty<GridViewBase, PercentLength>({
     name: "rowHeight",
     defaultValue: defaultRowHeight,
+    affectsLayout: true,
     equalityComparer: PercentLength.equals,
     valueConverter: PercentLength.parse,
     coerceValue: (target, value) => {
@@ -199,6 +200,7 @@ const defaultColWidth: PercentLength = "auto";
 export const colWidthProperty = new CoercibleProperty<GridViewBase, PercentLength>({
     name: "colWidth",
     defaultValue: PercentLength.parse("100"),
+    affectsLayout: true,
     equalityComparer: PercentLength.equals,
     valueConverter: PercentLength.parse,
     coerceValue: (target, value) => {
